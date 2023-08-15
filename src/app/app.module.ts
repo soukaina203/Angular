0d7576d1e5722d29a8695ed4d2048c6fc6785be2
soukaina203 <soukaina9mourabit@gmail.com>
2023-08-15 09:11:37 +0000

    basics

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,19 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { FirstComponent } from './first/first.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FirstComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+
   ],
   providers: [],
   bootstrap: [AppComponent]
